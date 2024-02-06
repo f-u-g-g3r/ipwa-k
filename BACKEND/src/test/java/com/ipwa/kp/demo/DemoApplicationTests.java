5a08ae9ea20e2ae92feb49f29a6ef908d48d5359
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2024-02-06 16:21:11 +0200

    init commit

---
+++ BACKEND/src/test/java/com/ipwa/kp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipwa.kp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

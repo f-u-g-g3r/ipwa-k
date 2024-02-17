9d0ebda185fe7dc4b1d0a4902a83942d1651eeee
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2024-02-17 15:28:26 +0200

    test

--- BACKEND/src/main/java/com/ipwa/kp/security/config/JwtService.java
+++ BACKEND/src/main/java/com/ipwa/kp/security/config/JwtService.java
@@ -40,6 +40,8 @@ public class JwtService {
         return claims.get("role", String.class);
     }
 
+    // test
+
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
